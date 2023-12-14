2cd77417769883922e2552f9da8ca9854b2a1ebb
sumeen99 <70104259+sumeen99@users.noreply.github.com>
2023-12-14 15:13:25 +0900

    init

---
+++ src/test/java/com/kbt1/ollilove/userservice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbt1.ollilove.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
