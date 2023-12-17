0d79c17a164db714f6a24a5b61ffe8c8938e9507
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2023-12-17 13:18:06 +0900

    chore: retest

--- src/main/java/com/kbt1/ollilove/userservice/Application.java
+++ src/main/java/com/kbt1/ollilove/userservice/Application.java
@@ -8,7 +8,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		System.out.println("ci cd test");
+		System.out.println("ci cd test2");
 	}
 
 }
